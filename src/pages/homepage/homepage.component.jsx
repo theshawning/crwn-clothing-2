0b0bd473a3dfd5a8bab187ed46bb8221d8c7b0e0
Shawn P Harrison <theshawningdev@gmail.com>
2020-10-21 13:20:12 +0100

    created basic components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const Hompage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Hompage;
